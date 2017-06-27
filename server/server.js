4022309e187d2eec7f9249513d2c4a770cde8cd7
Adedeji Ajiboye <ajibdeji@gmail.com>
2017-06-27 11:32:39 -0400

    added get / Todos

--- server/server.js
+++ server/server.js
@@ -13,12 +13,21 @@ app.post('/todos', (req, res) => {
         text: req.body.text
     });
 
+
     todo.save().then((doc) => {
         res.send(doc);
     }, (e) => {
         res.status(400).send(e);
         console.error(e);
     });
+});
+app.get('/todos', (req, res) => {
+    Todo.find()
+        .then((todos) => {
+            res.send({ todos });
+        }, (e) => {
+            res.status(400).send(e);
+        })
 });
 app.listen(3000, () => {
     console.log('Started on port 3000');
