04eeadb1171d6b2b8cb2b65b4957b777207f962c
Adedeji Ajiboye <ajibdeji@gmail.com>
2017-06-27 21:12:03 -0400

    add patch todos update

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 var { ObjectId } = require('mongodb');
 var { mongoose } = require('./db/mongoose.js');
@@ -66,7 +67,31 @@ app.delete('/todos/:id', (req, res) => {
     }).catch((e) => {
         res.status(400).send();
     });
-})
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectId.isValid(id)) {
+        return res.status(404).send({});
+    }
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+        if (!todo) {
+            return res.status(404).send({ error: 'Cannot find Todo' });
+        }
+        res.send({ todo });
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
